9b1427411ac88b9e49c192faa8fbda9fb77c740e
Yuriy Zelenko <yurazelenko@windowslive.com>
2017-10-06 00:50:18 +0300

    set and get weight and height

--- app/server/index.js
+++ app/server/index.js
@@ -2,14 +2,27 @@ const app = require("express")();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
-http.listen(3000, function(){
+http.listen(3000, function () {
 
 });
 
-io.on("connection", function(socket){
+let weight, height;
+let testTrain = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo excepturi, maiores officia saepe doloremque fugit, architecto culpa, iure illo ducimus animi molestiae, in praesentium eligendi corrupti soluta accusantium eaque fuga.";
+
+io.on("connection", function (socket) {
     console.log("connect");
+
+    socket.on("getWeight", function (data) {
+        weight = data;
+    });
+    socket.on("getHeight", function (data) {
+        height = data;
+    });
+    socket.on("getTrain", function () {
+        io.emit("getTrain", "Твоя вага: " + weight + "<br>Твій ріст: " + height + "<br>Тренування для тебе: " + testTrain);
+    });
 })
