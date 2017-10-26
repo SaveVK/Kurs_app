6ba8419bf3411f8318957d70852bfddde0b8dc11
Yuriy Zelenko <microsoftman535@gmail.com>
2017-10-26 21:55:36 +0300

    add routing

--- app/server/index.js
+++ app/server/index.js
@@ -5,25 +5,43 @@ const io = require("socket.io")(http);
 const mysql = require('mysql');
 
 app.use(express.static(__dirname));
+app.set("view engine", "ejs");
 
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: '1234',
-    database: 'trainingdb'
+    database: 'training_db'
 });
 connection.connect();
 
 
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.render("index", {
+        title: "SportTrainer"
+    });
+});
+
+app.get("/result", function (req, res) {
+    res.render("result", {
+        title: "Результати",
+        weight: weight,
+        height: height,
+        sex: sex,
+        age: age,
+        count: count,
+        level: level,
+        wrapCircumference: wrapCircumference,
+        goal: goal,
+        trainingdescription: trainingdescription
+    });
 });
 
 http.listen(3000, function () {
 
 });
 
-let sex, age, weight, height, count, level, wrapCircumference, goal;
+let sex, age, weight, height, count, level, wrapCircumference, goal, trainingdescription;
 
 io.on("connection", function (socket) {
     console.log("connect");
@@ -56,6 +74,9 @@ io.on("connection", function (socket) {
         connection.query("SELECT * FROM training WHERE weight = " + weight + " AND height = " + height, function (error, result, fields) {
             if (error) throw error;
             io.emit("getTrain", "Твоя вага: " + result[0].weight + "<br>Твій ріст: " + result[0].height + "<br>Стать: " + sex + "<br>Вік: " + age + "<br>Кількість тренувань: " + count + "<br>Рівень тренувань: " + level + "<br>Обхват зап'ястя: " + wrapCircumference + "<br>Ціль: " + goal + "<br>Тренування для тебе: " + result[0].trainingdescription);
+            weight = result[0].weight;
+            height = result[0].height;
+            trainingdescription = result[0].trainingdescription;
         });
     });
 });
